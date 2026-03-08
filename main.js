387eb4f826c3759ef5090d7912db47cbbe8866ba
Riskyandika <Riskyandika@gmail.com>
2026-03-08 20:52:16 +0800

    try to compare some method to defined and display a string in Javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+// I Try to comapare some method to define a string in JS
+// First method is using single quotes or single ticks
+let singleQuate = "Hallo world from single quates variable!";
+
+// Second one method is using double quates or double ticks
+let doubleQuates = "Hallo world from double quates variable!";
+// Between the first and second method there is no siginificant diffrence except the display look like and consistancy to use in out program
+
+let backTick = `Hello world from back tick quates, and here are the past version of me, first use single quoates "${singleQuate}" and second one is use double quotes "${doubleQuates}"`;
+
+// Try to display the result between three method i have defined
+console.log(singleQuate);
+console.log(doubleQuates);
+console.log(backTick);
